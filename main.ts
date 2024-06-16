4c540673ff671466edd5cd1dc2d1ea1d596417d9
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2024-06-16 20:44:55 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// Programm weiterschalten (0-4)
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     programm += 1
     if (programm > 4) {
@@ -18,8 +19,8 @@ function farbenzeigen () {
         strip.show()
         basic.pause(50)
     }
-    radio.sendString("" + (Boardnamen[BoardnamenPosition]))
     basic.pause(5)
+    radio.sendString("" + (Boardnamen[BoardnamenPosition]))
     BoardnamenPosition += 1
     if (BoardnamenPosition >= Boardnamen.length) {
         BoardnamenPosition = 0
@@ -43,7 +44,7 @@ input.onButtonPressed(Button.A, function () {
     } else if (programm == 3) {
         farbenzeigen()
     } else if (programm == 4) {
-        farbenzeigen()
+        farbenzeigen2()
     }
 })
 function kontrollbefehle (num: number) {
@@ -64,40 +65,44 @@ input.onButtonPressed(Button.AB, function () {
         strip.clear()
         strip.show()
     } else if (programm == 3) {
+        basic.showString("" + (Boardnamen.length))
         for (let Index = 0; Index <= Boardnamen.length - 1; Index++) {
             basic.showString("" + (Boardnamen[Index]))
         }
     } else if (programm == 4) {
+        basic.showString("" + (Boardnamen.length))
         for (let Index = 0; Index <= Boardnamen.length - 1; Index++) {
             basic.showString("" + (Boardnamen[Index]))
         }
     }
 })
+// im Programm 0:
+// Farben können auch mit dem Handlebit ausgelöst werden.
 radio.onReceivedString(function (receivedString) {
     if (programm == 0) {
         if (receivedString == "p-links") {
-            strip.showColor(informatiktheater.colors(NeoPixelColors.Red))
+            strip.showColor(informatiktheater.colors(NeoPixelColors.Blue))
             for (let index = 0; index < strip.length(); index++) {
                 basic.pause(20)
                 strip.shift(1)
                 strip.show()
             }
         } else if (receivedString == "b-links") {
-            strip.showColor(informatiktheater.colors(NeoPixelColors.Orange))
+            strip.showColor(informatiktheater.colors(NeoPixelColors.White))
             for (let index = 0; index < strip.length(); index++) {
                 basic.pause(20)
                 strip.show()
                 strip.shift(1)
             }
         } else if (receivedString == "p-rechts") {
-            strip.showColor(informatiktheater.colors(NeoPixelColors.Green))
+            strip.showColor(informatiktheater.colors(NeoPixelColors.Red))
             for (let index = 0; index < strip.length(); index++) {
                 basic.pause(20)
                 strip.show()
                 strip.shift(1)
             }
         } else if (receivedString == "b-rechts") {
-            strip.showColor(informatiktheater.colors(NeoPixelColors.White))
+            strip.showColor(informatiktheater.colors(NeoPixelColors.Orange))
             for (let index = 0; index < strip.length(); index++) {
                 basic.pause(20)
                 strip.show()
@@ -118,6 +123,13 @@ radio.onReceivedString(function (receivedString) {
         } else if (receivedString == "schwarz") {
             strip.showColor(informatiktheater.colors(NeoPixelColors.Black))
             strip.show()
+        } else if (receivedString == "m-links") {
+            strip.showRainbow(1, 255)
+            strip.show()
+        } else if (receivedString == "m-rechts") {
+            strip.showColor(informatiktheater.colors(NeoPixelColors.Black))
+            strip.showRainbow(255, 1)
+            strip.show()
         }
     } else if (programm == 3) {
         if (control.deviceName() == receivedString) {
@@ -125,7 +137,7 @@ radio.onReceivedString(function (receivedString) {
         }
     } else if (programm == 4) {
         if (control.deviceName() == receivedString) {
-            farbenzeigen()
+            farbenzeigen2()
         }
     }
 })
@@ -145,25 +157,51 @@ input.onButtonPressed(Button.B, function () {
     	
     }
 })
+// wen es nicht geklappt hat, kann hier manuell der eigene Namen gesendet werden
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    if (login == 0) {
-        radio.sendValue(control.deviceName(), 100)
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        login = 1
-        basic.showNumber(Boardnamen.length)
-    }
-    programm = 3
-    led.plot(programm, programm)
+    radio.sendValue(control.deviceName(), 200)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    basic.showNumber(Boardnamen.length)
 })
 radio.onReceivedValue(function (name, value) {
-    if (value == 100) {
+    if (value == 200) {
         Boardnamen.push(name)
         music.play(music.tonePlayable(494, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
-        basic.showNumber(Boardnamen.length)
-    } else if (false) {
-    	
-    } else {
-    	
+        basic.showIcon(IconNames.Yes)
+    }
+    if (name == "x-rechts" && value < 10) {
+        strip.showColor(informatiktheater.colors(NeoPixelColors.Red))
+        strip.show()
+    }
+    if (programm == 0) {
+        if (name == "x-rechts" && value > 10) {
+            strip.showColor(informatiktheater.colors(NeoPixelColors.Orange))
+            strip.show()
+        }
+        if (name == "y-rechts" && value < 10) {
+            strip.showColor(informatiktheater.colors(NeoPixelColors.Yellow))
+            strip.show()
+        }
+        if (name == "y-rechts" && value > 10) {
+            strip.showColor(informatiktheater.colors(NeoPixelColors.Purple))
+            strip.show()
+        }
+        if (name == "x-links" && value > 10) {
+            strip.showColor(informatiktheater.colors(NeoPixelColors.Blue))
+            strip.show()
+        }
+        if (name == "x-links" && value < 10) {
+            strip.showColor(informatiktheater.colors(NeoPixelColors.Violet))
+            strip.show()
+        }
+        if (name == "y-links" && value > 10) {
+            strip.showColor(informatiktheater.colors(NeoPixelColors.Green))
+            strip.show()
+        }
+        if (name == "y-links" && value < 10) {
+            strip.showColor(informatiktheater.colors(NeoPixelColors.White))
+            strip.show()
+        }
     }
 })
 function farbendefinieren (num: number) {
@@ -184,20 +222,40 @@ function farbenzeigen2 () {
 }
 let farben: string[] = []
 let befehle: string[] = []
-let BoardnamenPosition = 0
 let programm = 0
+let BoardnamenPosition = 0
 let Boardnamen: string[] = []
-let login = 0
 let strip: informatiktheater.Strip = null
 strip = informatiktheater.create(HiwonderPins.P2, 60, PowerSource.Intern)
 radio.setGroup(1)
 // ist der Stab bereis eingeloggt?
-login = 0
-// Liste erstellen mit allen Boardnamen im Netzwerk
+let login = 0
+// Liste erstellen für alle Boardnamen im Netzwerk
 Boardnamen = []
+BoardnamenPosition = 0
 programm = 0
 kontrollbefehle(1)
 farbendefinieren(1)
+basic.showString(control.deviceName())
+for (let Index = 0; Index <= 1; Index++) {
+    strip.setPixelColorRange(0, informatiktheater.colors(NeoPixelColors.Blue), 1)
+    for (let index = 0; index < 8; index++) {
+        strip.show()
+        strip.rotate(5)
+        basic.pause(100)
+    }
+    // Wertepaar Gerätename und 200 schreibt Boardnamen ins Array Boardnamen
+    radio.sendValue(control.deviceName(), 200)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    strip.clear()
+    strip.show()
+}
+if (Boardnamen.length > 0) {
+    // andere Boards gefunden
+    basic.showIcon(IconNames.Yes)
+} else {
+    basic.showIcon(IconNames.Sad)
+}
 basic.forever(function () {
     if (programm == 0) {
     	
